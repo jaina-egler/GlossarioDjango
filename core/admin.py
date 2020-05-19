130fced440156051b8f8c2584d8c3b8d97af2524
jc-aaa <jainadadycardoso@gmail.com>
2020-05-18 22:05:05 -0400

    Adicionei os models e colocquei no admin, ainda tentando mexer com imagens

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from core.models import Glossario
 
+admin.site.register(Glossario)
 # Register your models here.
--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+class Glossario(models.Model):
+    image = models.ImageField(upload_to="uploads/",height_field=100, width_field=200,max_length = 100,),
+    word = models.CharField("Termo",max_length=50)
+    areaC =(
+        ('Ciências exatas e da terra','Ciências exatas e da terra'),
+       ( 'Ciências biológicas','Ciências biológicas'),
+        ('Engenharias','Engenharias'),
+        ('Ciências da saúde','Ciências da saúde'),
+        ('Ciências agrárias','Ciências agrárias'),
+        ('Ciências sociais e aplicadas','Ciências sociais e aplicadas'),
+       ( 'Ciências humanas','Ciências humanas'),
+       ( 'Linguística, letras e artes', 'Linguística, letras e artes'),
+        ('Outros','Outros')
+    )  
+    area = models.CharField("Área", choices=areaC, max_length=30)
+    exp = models.CharField("Expressão",max_length=50)
+    defin = models.TextField("Definição",max_length=150)   
 # Create your models here.
